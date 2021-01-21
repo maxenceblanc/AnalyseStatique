004b7175f4f8af4c318534948ab625f83731a86d
Maxence Blanc <maxence.blanc@yahoo.com>
2021-01-21 11:43:02 +0100

    object filling

--- reader.py
+++ reader.py
@@ -7,6 +7,7 @@
 ########################
 
 # IMPORTS
+import json
 
 # CUSTOM IMPORTS
 
@@ -22,21 +23,23 @@
 ###################| CLASSES |######################
 ####################################################
 
-# class Graph():
-
-#     def __init__():
-#         pass
-
 class Block():
 
-    def __init__(id):
+    def __init__(self, id, instructions=[], block_before=[], block_after=[]):
 
         self.id = id
 
-        self.expressions = []
+        self.instructions = instructions
 
-        self.block_before = []
-        self.block_after = []
+        self.block_before = block_before
+        self.block_after = block_after
+
+    def __repr__(self):
+        chaine = f"Block {self.id}\nInstructions:\n"
+        for instruction in self.instructions:
+            chaine += f"    {instruction}\n"
+        chaine += f"Before: {self.block_before}\nAfter:  {self.block_after}\n"
+        return chaine
 
 ####################################################
 ##################| FUNCTIONS |#####################
@@ -52,25 +55,18 @@ def parseFile(filename: str):
     """ 
     
     with open(filename) as graph_file:
-        data = [line.rstrip() for line in graph_file.readlines()]
-        print(data)
+        data =  json.load(graph_file)
 
 
-    block_list = []
+    block_dict = {}
 
-    for line in data:
-        pass
+    for block_id in data:
+        new_block = Block(block_id, data[block_id]["instructions"], data[block_id]["pred"], data[block_id]["succ"])
+        block_dict[block_id] = new_block
 
-    return block_list
 
+    return block_dict
 
-def addNetwork(data, blocks):
-    """  TODO
-
-    INPUTS: 
-    OUTPUT:
-    """ 
-    pass
 
 ####################################################
 ###################| CONSTANTS |####################
@@ -84,4 +80,7 @@ GRAPH_FILE = "graphs/graph.json"
 
 if __name__ == '__main__':
 
-    block_list = parseFile(GRAPH_FILE)
+    block_dict = parseFile(GRAPH_FILE)
+
+    for block_id in block_dict:
+        print(block_id, block_dict[block_id])
